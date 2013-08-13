9c1697a1014983c378038c9e9950753b622bbcf5
manutero <manutero@gmail.com>
2013-08-13 18:18:32 +0200

    Empty iOs project created

---
+++ Team_Creator_App/Team_Creator_App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Team_Creator_App
+//
+//  Created by Manuel Artero Anguita on 13/08/13.
+//  Copyright (c) 2013 Manuel Artero Anguita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Team_Creator_App/Team_Creator_App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Team_Creator_App
+//
+//  Created by Manuel Artero Anguita on 13/08/13.
+//  Copyright (c) 2013 Manuel Artero Anguita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
