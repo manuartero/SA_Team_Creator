73e607d9a4e86d0371044ec519c3502abd6c8060
manutero <manutero@gmail.com>
2014-01-26 19:21:33 +0100

    empty rails project

---
+++ Team_Creator_server/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamCreatorServer::Application.config.secret_key_base = 'ebb4692c5c0c9d1afb39901afee3096e23ace1558d21de7760819dd736c3fad49a32ec39cf07317ed89254af4d662a086288e32df37c89e2d239d7498359adc1'
---
+++ Team_Creator_server/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeamCreatorServer::Application.config.session_store :cookie_store, key: '_Team_Creator_server_session'
